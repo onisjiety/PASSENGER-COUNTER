b42a274ed9341407d41d84203d306b0f503eef8d
onisjiety <onisjiety@gmail.com>
2023-01-26 20:10:06 +0800

    PASSENGERS APP

---
+++ index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+let saveEl = document.getElementById("save-el");
+
+function save() {
+    let display = count + " - ";
+    saveEl.textContent += display;
+    countEl.textContent = 0;
+    count = 0;
+}
